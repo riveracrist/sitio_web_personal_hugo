13f309765ee02858589d2a8edd89a32e65ee162b
riveracrist <riveracrist@gmail.com>
2023-12-12 16:20:22 -0500

    aregando archivos prueba hugo

---
+++ content/_index.md
@@ -0,0 +1,9 @@
+---
+header_headline: Jane Doe
+header_image: images/cover-image.jpg
+header_logo: images/chef-hat.png
+header_subheadline: <b>Nutrition</b> Coach & <b>Chef</b> Consultant
+header_use_video: false
+nav_to_top_title: Return To Top
+nav_to_top_weight: last
+---
---
+++ setup-R.R
@@ -0,0 +1,17 @@
+#blogdown::new_site(theme = "zjedi/hugo-scroll")
+blogdown::build_site() # para cambios grandes
+blogdown::serve_site()
+blogdown::stop_server()
+
+
+# Guía --> https://carpentries-incubator.github.io/blogging-with-hugo-and-github-pages/
+# Guía --> https://www.drdataking.com/post/building-your-website-with-hugo-and-rmarkdown/
+ 
+# Guías de Hugo --> Quarto
+# https://silviacanelon.com/about/
+# https://www.andreashandel.com/posts
+# https://www.paulrjohnson.net/
+# https://university.wowchemy.com/getting-started/customize/
+
+# Plantillas Hugoblox
+# https://hugoblox.com/templates/
